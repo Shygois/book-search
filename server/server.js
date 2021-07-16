29eeee9cfa67fd1947400e12a4d5da2bcec3c366
Shygois <shayana.gois@att.net>
2021-07-15 21:17:44 -0700

    updated server

--- server/server.js
+++ server/server.js
@@ -1,39 +1,32 @@
 const express = require('express');
-const { authMiddleware } = require('./utils/auth');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
-// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-// integrate our Apollo server with the Express application as middle
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 const path = require('path');
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(_dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
